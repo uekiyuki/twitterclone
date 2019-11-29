7d48f665adc6c3d812fbf42484f6370eb9c3cac8
yuki ueki <karoo4413@gmail.com>
2019-11-29 17:46:19 +0900

    編集機能を追加

--- app/controllers/heiheis_controller.rb
+++ app/controllers/heiheis_controller.rb
@@ -1,4 +1,6 @@
 class HeiheisController < ApplicationController
+  before_action :set_heihei, only: [:show, :edit, :update]
+
   def index
     @heiheis = Heihei.all
   end
@@ -8,17 +10,42 @@ class HeiheisController < ApplicationController
   end
 
   def create
-    Heihei.create(heihei_params)
-    redirect_to new_heihei_path
+    @heihei = Heihei.new(heihei_params)
+    if @heihei.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to heiheis_path, notice: "Heihei!を作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    
+    end
   end
+
 
   def show
     @heihei = Heihei.find(params[:id])
+  end
+
+  def edit
+    @heihei = Heihei.find(params[:id])
+  end
+
+  def update
+    @heihei = Heihei.find(params[:id])
+    if @heihei.update(heihei_params)
+      redirect_to heiheis_path, notice: "Heiheiを編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
 
   def heihei_params
     params.require(:heihei).permit(:content)
+  end
+
+  def set_heihei
+    @heihei= Heihei.find(params[:id])
   end
 end
--- app/models/heihei.rb
+++ app/models/heihei.rb
@@ -1,2 +1,4 @@
 class Heihei < ApplicationRecord
+  validates :content, presence: true
+  validates :content,    length: { in: 1..140 } 
 end
