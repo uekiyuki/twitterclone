36d9ae52450a0e962f4342dca5207d386d5d0145
yuki ueki <karoo4413@gmail.com>
2019-11-29 16:04:52 +0900

    投稿一覧表示機能を追加

--- app/controllers/heiheis_controller.rb
+++ app/controllers/heiheis_controller.rb
@@ -1,7 +1,20 @@
 class HeiheisController < ApplicationController
   def index
+    @heiheis = Heihei.all
   end
   # 追記する
   def new
+    @heihei = Heihei.new
+  end
+
+  def create
+    Heihei.create(heihei_params)
+    redirect_to new_heihei_path
+  end
+
+  private
+
+  def heihei_params
+    params.require(:heihei).permit(:content)
   end
 end
